fd10ee97b670f00f9aec5469a5cbbfbf90c8e0be
BiancaGlavan <bianca.glavan@yahoo.ro>
2023-03-08 18:15:20 +0200

    added api calls for register, login and user profile

--- src/redux/features/apiSlice.ts
+++ src/redux/features/apiSlice.ts
@@ -1,12 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { IUser } from "./authSlice";
 
+interface IAuthResponse {
+  access_token: string;
+  user: IUser;
+}
 
+interface IMyProfileResponse {
+  profile: IUser;
+}
 
 export const backendApi = createApi({
   reducerPath: "backendapi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://eager-dog-tie.cyclic.app/api/",
+    baseUrl: "http://localhost:5000/api/",
     prepareHeaders: (headers, { getState }) => {
       const state = getState() as RootState;
       const token = state.auth.token;
@@ -16,10 +24,27 @@ export const backendApi = createApi({
       return headers;
     },
   }),
-  tagTypes: [],
+  tagTypes: ["Profile"],
 
   endpoints: (builder) => ({
-   
+   registerUser: builder.mutation<IAuthResponse, { data: Partial<IUser>}>({
+    query: ({ data }) => ({
+      url: "auth/register",
+      method: "POST",
+      body: data,
+    }),
+   }),
+   loginUser: builder.mutation<IAuthResponse, { data: Partial<IUser>}>({
+    query: ({ data }) => ({
+      url: "auth/login",
+      method: "POST",
+      body: data,
+    }),
+   }),
+   getMyProfile: builder.query<IMyProfileResponse, {}>({
+    query: () => "auth/profile",
+    providesTags: ["Profile"],
+   }),
   }),
 });
 export const {
